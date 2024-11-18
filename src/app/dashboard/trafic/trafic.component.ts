65a99dd232a9bfe59d52161585d59b19a8f134a6
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-11-18 13:33:11 +0100

    refactor: add separate components

---
+++ src/app/dashboard/trafic/trafic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trafic',
+  standalone: true,
+  imports: [],
+  templateUrl: './trafic.component.html',
+  styleUrl: './trafic.component.css'
+})
+export class TraficComponent {
+
+}
