c89e7f38678182536f528ce2eba876a5ab78ea30
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-11-19 14:26:09 +0100

    refactor: add control reusable component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   // selector: 'app-button',
-  selector: 'button[appButton]', //* atribute selector
+  selector: 'button[appButton], a[appButton]', //* atribute selector
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
